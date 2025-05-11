91108f80d1a867da379b3205db31a141e2418291
Michael Shaba <shabamichael@outlook.com>
2025-05-10 18:17:08 -0700

    Commit application after configuring and installing Zipkin

--- services/product/src/main/java/com/michael/ecommerce/product/ProductController.java
+++ services/product/src/main/java/com/michael/ecommerce/product/ProductController.java
@@ -4,7 +4,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
